83d11668fd2468600e9de2fc510ca6abf429a340
jtrds8fn <jsjcaelpenaloza@hotmail.com>
2024-05-07 05:34:39 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-function permuteUnique(nums) {
+function subsetsWithDup(nums) {
   const result = [];
   nums.sort((a, b) => a - b);
-  backtrack([], new Array(nums.length).fill(false));
+  backtrack(0, []);
   return result;
-  function backtrack(current, used) {
-    if (current.length === nums.length) {
-      result.push([...current]);
-      return;
-    }
-    for (let i = 0; i < nums.length; i++) {
-      if (used[i] || (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]))
-        continue;
+  function backtrack(start, current) {
+    result.push([...current]);
+    for (let i = start; i < nums.length; i++) {
+      if (i > start && nums[i] === nums[i - 1]) continue;
       current.push(nums[i]);
-      used[i] = true;
-      backtrack(current, used);
+      backtrack(i + 1, current);
       current.pop();
-      used[i] = false;
     }
   }
 }
