6ce7e582287a96ebf3803b2febeb9f6fd8651b71
jtrds8fn <jsjcaelpenaloza@hotmail.com>
2024-05-10 10:18:02 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-function buildTree(inorder, postorder) {
-  if (!inorder.length || !postorder.length) return null;
-  const rootVal = postorder[postorder.length - 1];
-  const root = new TreeNode(rootVal);
-  const mid = inorder.indexOf(rootVal);
-  root.left = buildTree(inorder.slice(0, mid), postorder.slice(0, mid));
-  root.right = buildTree(
-    inorder.slice(mid + 1),
-    postorder.slice(mid, postorder.length - 1),
-  );
-  return root;
+function numIslands(grid) {
+  if (grid.length === 0 || grid[0].length === 0) return 0;
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let islands = 0;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === "1") {
+        islands++;
+        dfs(i, j);
+      }
+    }
+  }
+  return islands;
+  function dfs(row, col) {
+    if (
+      row < 0 ||
+      row >= rows ||
+      col < 0 ||
+      col >= cols ||
+      grid[row][col] !== "1"
+    )
+      return;
+    grid[row][col] = "0";
+    dfs(row + 1, col);
+    dfs(row - 1, col);
+    dfs(row, col + 1);
+    dfs(row, col - 1);
+  }
 }
